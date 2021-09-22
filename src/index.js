a8a538f8c7bdc723708d710a07e18e2ac8e8702a
Basit Ali <basitk41@gmail.com>
2021-09-22 21:32:00 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React, { useState, useContext } from "react";
 const ContextState = React.createContext(["", () => {}]);
 export const useSimple = () => useContext(ContextState)
-const ContextProvider = ({ initialState, children }) => {
-  const [state, setState] = useState(initialState);
-  return <ContextState.Provider value={[state, setState]}>{children}</ContextState.Provider>
-};
+const ContextProvider = ({ initialState, children }) => <ContextState.Provider value={useState(initialState)}>{children}</ContextState.Provider>
 export default ContextProvider;
